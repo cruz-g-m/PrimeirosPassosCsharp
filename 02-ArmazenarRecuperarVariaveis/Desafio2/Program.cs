ca024cb709151b8739fa8cfa8c22fa6aab6014bc
guilherme cruz <guilherme.m.cruz4@gmail.com>
2022-09-28 16:44:51 -0300

    AdcComentarios

--- 02-ArmazenarRecuperarVariaveis/Desafio2/Program.cs
+++ 02-ArmazenarRecuperarVariaveis/Desafio2/Program.cs
@@ -1,4 +1,16 @@
-﻿string name = "Bob";
+﻿/*Armazene os seguintes valores da saída em variáveis:
+Bob
+3
+34.4
+Essas variáveis devem receber nomes que reflitam sua finalidade.
+
+Selecione o tipo de dados correto para cada uma das variáveis com base no tipo de dados que ela manterá.
+
+Por fim, você combinará as variáveis com cadeias de caracteres literais passadas para uma 
+série de comandos Console.Write() para formar a mensagem completa:
+"Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius." */
+
+string name = "Bob";
 int message = 3;
 decimal celsius = 34.4m;
 
