e20a8e93be830adc8d383d96db0d05f85f84f805
guilherme cruz <guilherme.m.cruz4@gmail.com>
2022-09-28 16:39:26 -0300

    AdcComentarios

--- 02-ArmazenarRecuperarVariaveis/02-DeclararVariaveis/Program.cs
+++ 02-ArmazenarRecuperarVariaveis/02-DeclararVariaveis/Program.cs
@@ -1,10 +1,20 @@
-﻿string firstName = "bob";
+﻿/* Para criar uma nova variável, primeiro você deve declarar o tipo de dados da variável e, 
+em seguida, da um nome a ela */
+
+/*Para atribuir um valor a uma variável, use o operador de atribuição, que é um caractere único igual a =.
+Exemplos de nomes de variaveis*/
+
+string firstName = "bob"; 
+//A atribuição ocorre da direita para a esquerda.
 char userOption = 'Y';
 int gameScore = 7;
 decimal particlesPerMillion = 10.7m;
 bool processedCustumer = false;
 
+//Você deve definir uma variável como um valor antes de poder obter o valor da variável. 
+
 Console.WriteLine("");
+//Para recuperar um valor de uma variável, basta usar o nome da variável.
 Console.WriteLine(firstName);
 
 Console.WriteLine("");
