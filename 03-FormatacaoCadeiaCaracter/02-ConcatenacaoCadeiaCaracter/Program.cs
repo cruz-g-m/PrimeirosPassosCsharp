a2d2ff1109c7d0210e1c32a02d283f1749f05165
guilherme cruz <guilherme.m.cruz4@gmail.com>
2022-09-29 09:17:15 -0300

    AdcComentarios

--- 03-FormatacaoCadeiaCaracter/02-ConcatenacaoCadeiaCaracter/Program.cs
+++ 03-FormatacaoCadeiaCaracter/02-ConcatenacaoCadeiaCaracter/Program.cs
@@ -1,11 +1,12 @@
-﻿//Ultimo teste
+﻿/*Para concatenar duas cadeias de caracteres,
+use o operador de concatenação de cadeia de caracteres, que é o símbolo de adição +.*/
 Console.WriteLine("");
 string firstName = "Bob";
 string message = "Hello " + firstName;
 Console.WriteLine(message);
 
 Console.WriteLine("");
-
+//Você pode executar várias operações de concatenação na mesma linha de código.
 string secondName = "Steve";
 string greeting = "Hello";
 string message2 = greeting + " " + secondName + "!";
--- 03-FormatacaoCadeiaCaracter/03-InterpolacaoCadeiaCaracter/Program.cs
+++ 03-FormatacaoCadeiaCaracter/03-InterpolacaoCadeiaCaracter/Program.cs
@@ -1,11 +1,12 @@
 ﻿Console.WriteLine("");
-
+/*Uma expressão de interpolação é uma variável cercada por um símbolo de chave de abertura e fechamento { }.
+A cadeia de caracteres literal se torna um modelo quando ele é prefixado pelo caractere $. */
 string firstName = "bob";
 string message = $"Hello {firstName}!";
 Console.WriteLine(message);
 
 Console.WriteLine("");
-
+//Você pode executar várias operações de interpolação na mesma linha de código.
 string secondName = "Steve";
 string greeting = "Hello";
 string message2 = $"{greeting} {secondName} !";
@@ -18,7 +19,7 @@ string greeting2 = "Helo";
 Console.WriteLine($"{greeting2} {thirdName}");
 
 Console.WriteLine("");
-
+//Você pode usar o símbolo @ de prefixo literal textual e o símbolo $ de interpolação de cadeia de caracteres juntos.
 string projectName = "First-Project";
 Console.WriteLine($@"C:\Output\{projectName}\Data");
 
