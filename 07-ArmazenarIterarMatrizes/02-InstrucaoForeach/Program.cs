e749ce51200fd6560a30705563507496d641909a
Guilherme Cruz <67871173+cruz-g-m@users.noreply.github.com>
2022-09-30 09:03:25 -0300

    FirstCommit

--- 07-ArmazenarIterarMatrizes/02-InstrucaoForeach/Program.cs
+++ 07-ArmazenarIterarMatrizes/02-InstrucaoForeach/Program.cs
@@ -1,1 +1,69 @@
-﻿Console.WriteLine("aa");
+﻿/*
+A instrução foreach realiza um loop em cada elemento da matriz, executando 
+o bloco de código abaixo da declaração e substituindo o valor em uma variável 
+temporária pelo valor da matriz representada pelo loop atual.
+*/
+
+Console.WriteLine("");
+
+string[] names = { "Bob", "Conrad", "Grant" };
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+Console.WriteLine("");
+
+/*
+Abaixo da palavra-chave foreach, o bloco de código que contém Console.WriteLine(name); 
+será executado uma vez para cada elemento da matriz names. Como o runtime do .NET 
+executa um loop em cada elemento da matriz, o valor armazenado no elemento atual da 
+matriz names será atribuído à variável temporária name para facilitar o acesso 
+dentro do bloco de código.
+*/
+
+//===============================================//=====================================
+
+/*
+Criar uma matriz de int que armazenará o número de produtos finais em cada compartimento.
+*/
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+
+/*
+Defina uma nova variável que representa a soma de todos os produtos finais no depósito. 
+Não se esqueça de adicioná-la fora da instrução foreach.
+*/
+int sum = 0;
+
+/*
+Acima da instrução foreach, declare e inicialize uma nova variável do tipo int para armazenar 
+o número atual do compartimento cujo inventário está sendo processado.
+*/
+int bin = 0;
+
+/*
+Em seguida, adicione uma instrução foreach que itera em cada elemento da matriz inventory, 
+atribuindo temporariamente o valor do elemento atual à variável items. Seu código deve 
+corresponder à listagem a seguir.
+
+Dentro da instrução foreach, adicione a linha de código, que acrescenta o valor 
+atual armazenado em items à variável sum.
+
+Dentro do bloco de código foreach, incremente o compartimento cada vez que o bloco de 
+código for executado.
+
+Por fim, dentro do foreach, exiba o compartimento, a contagem de produtos finais e o 
+total atual de produtos finais em uma mensagem bem formatada.
+*/
+
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+
+Console.WriteLine($"We have {sum} items in inventory.");
+
+Console.WriteLine("");
---
+++ 07-ArmazenarIterarMatrizes/Desafio7/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Anteriormente, decidimos escrever código para armazenar IDs de pedidos possivelmente 
+fraudulentos. Esperamos encontrar pedidos fraudulentos o quanto antes e sinalizá-los 
+para uma análise mais profunda.
+
+Nossa equipe detectou um padrão. Pedidos que começam com a letra "B" têm um número de 
+fraudes 25 vezes maior que o normal. Vamos escrever um novo código para gerar a ID de 
+novos pedidos, em que a ID do pedido começa com a letra "B". Isso será usado por nossa
+equipe de fraudes para investigar ainda mais.
+
+Declarar e inicializar uma nova matriz para conter IDs de pedidos falsas.
+
+B123
+C234
+A345
+C15
+B177
+G3003
+C235
+B179
+
+Use uma instrução foreach para iterar em cada elemento da matriz que você acabou 
+de declarar e inicializar.
+
+Se a ID de Pedido falsa começar com "B", imprima-a na saída.
+Avalie cada elemento da matriz. Identifique e imprima para gerar as IDs de pedidos 
+possivelmente fraudulentas procurando os pedidos que começam com a letra "B". A 
+saída deve corresponder ao seguinte:
+
+B123
+B177
+B179
+
+Para determinar se um elemento começa ou não com a letra "B", use o método String.StartsWith().
+
+*/
+
+Console.WriteLine("");
+
+string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
+
+Console.WriteLine("");
